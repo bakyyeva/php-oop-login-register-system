2a168d986ff637ea919c5f921b0c8adeb96543f0
uzukb <uzukbakyyeva@gmail.com>
2022-08-03 04:16:28 +0300

    Home page

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php  
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <header>
+        <div class="first">
+            <a href="index.php"> 
+                <p>Uzuk</p> 
+                <p style="font-size: 1.2rem; margin-top:0;">BAKYYEVA</p>
+            </a>
+        </div>
+        <nav>
+           <ul class="nav-list">
+            <li> <a href="index.php">HOME</a> </li>
+            <li> <a href="#">ABOUT US</a> </li>
+            <li> <a href="#">CONTACT</a> </li>
+            <li> <p style="font-size: 3rem;"> | </p> </li>       
+            <li> <a href="login.php">LOGIN</a> </li>
+            <li> <a href="signup.php">SIGN UP</a> </li>
+           </ul>
+        </nav>
+    </header>
+    <main>
+       HOME PAGE
+    </main>
+</body>
+</html>
